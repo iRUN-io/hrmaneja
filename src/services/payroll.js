5f7fc8d01cc1dfbecd5c204d3a8d821620724de9
Godfred Akpan <godfredakpan@gmail.com>
2022-04-20 15:39:03 +0100

    done payroll services

---
+++ src/services/payroll.js
@@ -0,0 +1,53 @@
+import request from 'umi-request';
+import { API_URL, USER_TOKEN } from '../config/config';
+
+export async function getAllPayrolls(userId, ref) {
+  return request(`${API_URL}/api/payroll/${userId}?ref=${ref}`, {
+    method: 'get',
+    headers: {
+       'Content-Type': 'application/json',
+       'Authorization': USER_TOKEN,
+    },
+    params: {
+      id: userId,
+      ref: ref,
+    },
+  });
+}
+
+export async function createPayroll(body, userId) {
+  return request(`${API_URL}/api/payroll/${userId}`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': USER_TOKEN,
+    },
+    body: JSON.stringify(body),
+  });
+}
+
+export async function updatePayroll(body, userId, payrollId) {
+  return request(`${API_URL}/api/payroll/${userId}?id=${payrollId}`, {
+    method: 'patch',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': USER_TOKEN,
+    },
+    body: JSON.stringify(body),
+  });
+}
+
+
+export async function deletePayroll(userId, payrollId, ref) {
+  return request(`${API_URL}/api/payroll/${userId}?id=${payrollId}&ref=${ref}`, {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': USER_TOKEN,
+    },
+    params: {
+      id: userId,
+      ref: ref,
+    },
+  });
+}
