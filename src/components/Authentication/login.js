3f470040dc3ccaa9b29662d7258bca1c7aa0d754
Godfred Akpan <godfredakpan@gmail.com>
2022-07-18 22:08:29 +0100

    change response format

--- src/components/Authentication/login.js
+++ src/components/Authentication/login.js
@@ -31,7 +31,7 @@ const Login = () => {
 		try {
 			setProgress(50);
 			setResponse(response);
-			if (loggedResponse.status === 200) {
+			if (loggedResponse.data) {
 				setProgress(100);
 				toast.success(response.message)
 				sendEmail(response.data.emailAddress, response.data.name, emailCase.userLoggedIn);
