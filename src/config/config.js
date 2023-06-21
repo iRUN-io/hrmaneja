d65945cc32ae3cbfbedb74a08e82735a5e186eba
Godfred Akpan <godfredakpan@gmail.com>
2023-06-21 17:12:46 +0100

    update API Endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import { getUser } from './common';
 
-export const API_URL = 'http://hrmaneja.eu-4.evennode.com/api/v1';
+export const API_URL = 'https://hrmaneja.eu-4.evennode.com/api/v1';
 // export const API_URL = 'http://localhost:5050/api/v1';
 export const USER_ID = getUser.id;
 export const USER_TOKEN = getUser.token;
